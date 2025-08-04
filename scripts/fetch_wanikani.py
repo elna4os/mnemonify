5069138d5b5959d9f4e13ad0e8d3df985a620ada
alex <alex@air13.local>
2025-08-05 02:09:39 +0900

    Add script to fetch data from WaniKani API

---
+++ scripts/fetch_wanikani.py
@@ -0,0 +1,51 @@
+import json
+import os
+import time
+from pathlib import Path
+from typing import Any, Dict, List
+
+import requests
+import yaml
+from dotenv import load_dotenv
+from loguru import logger
+
+
+def fetch_subjects(
+    base_url: str,
+    headers: Dict[str, Any],
+    sleep_time: int = 3
+) -> List[Dict[str, Any]]:
+    subjects_list = []
+    url = base_url
+    while url:
+        resp = requests.get(url, headers=headers)
+        resp.raise_for_status()
+        data = resp.json()
+        logger.info(f"Fetched {len(data['data'])} subjects from {url}")
+        subjects_list.extend(data["data"])
+        url = data["pages"]["next_url"]
+        if url:
+            time.sleep(sleep_time)
+    logger.info(f"Total subjects fetched: {len(subjects_list)}")
+
+    return subjects_list
+
+
+if __name__ == "__main__":
+    try:
+        with open("params.yaml", "r") as f:
+            params = yaml.safe_load(f)
+        load_dotenv()
+        api_token = os.getenv("WANIKANI_API_TOKEN")
+        headers = {"Authorization": f"Bearer {api_token}"}
+        subjects_list = fetch_subjects(
+            base_url=params["fetch"]["base_url"],
+            headers=headers
+        )
+        Path(params["fetch"]["out_file"]).parent.mkdir(parents=True, exist_ok=True)
+        with open(params["fetch"]["out_file"], "w", encoding="utf-8") as f:
+            json.dump(subjects_list, f, ensure_ascii=False)
+        logger.info(f"Subjects saved to {params['fetch']['out_file']}")
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        raise e
